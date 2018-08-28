d20d577d3eac964d108cdc974e41049f61233554
lachlan mackenzie <lachlanrm@gmail.com>
2018-08-28 16:34:12 -0700

    Add feedAndMultiply method with storage pointer

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -3,5 +3,10 @@ pragma solitity ^0.4.24
 import "./zombiefactory"
 
 contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId]; 
+  }
     
 }
