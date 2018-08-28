5e1bc55991fc0fbe72dbc7c0125fcbc9122561b1
lachlan mackenzie <lachlanrm@gmail.com>
2018-08-28 13:49:14 -0700

    Add a zombie creation event

--- Contract.sol
+++ Contract.sol
@@ -11,8 +11,11 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    event NewZombie(uint zombieId, string name, uint dna);
+
     function _createZombie ( string _name, uint _dna ) private {
-        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string _str) private view returns (uint) {
