f41fe8933f0df22583b0e9c1d70ab5a2493fe436
lachlan mackenzie <lachlanrm@gmail.com>
2018-08-28 10:45:18 -0700

    Add struct for zombie

--- Contract.sol
+++ Contract.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.4.24;
 contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 }
