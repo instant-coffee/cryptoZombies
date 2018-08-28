f035742394c55ce3ec3ae3efb1df76a3fddf12bf
lachlan mackenzie <lachlanrm@gmail.com>
2018-08-28 15:06:57 -0700

    Add emit for event

--- Contract.sol
+++ Contract.sol
@@ -15,7 +15,7 @@ contract ZombieFactory {
 
     function _createZombie ( string _name, uint _dna ) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        NewZombie(id, _name, _dna);
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string _str) private view returns (uint) {
